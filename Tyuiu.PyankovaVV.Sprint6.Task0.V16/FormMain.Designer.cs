ea950b86858517f94e36aa0b9288374d4ab182e8
Tyuiu.PyankovaVV@outlook.com <vika.pyankova04@mail.ru>
2023-12-03 19:26:19 +0500

    Task0

---
+++ Tyuiu.PyankovaVV.Sprint6.Task0.V16/FormMain.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace Tyuiu.PyankovaVV.Sprint6.Task0.V16
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonPerform = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonPerform
+            // 
+            this.buttonPerform.Location = new System.Drawing.Point(698, 372);
+            this.buttonPerform.Name = "buttonPerform";
+            this.buttonPerform.Size = new System.Drawing.Size(143, 41);
+            this.buttonPerform.TabIndex = 0;
+            this.buttonPerform.Text = "Выполнить";
+            this.buttonPerform.UseVisualStyleBackColor = true;
+            this.buttonPerform.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(890, 450);
+            this.Controls.Add(this.buttonPerform);
+            this.MinimizeBox = false;
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Спринт6 | Таск 0 | Вариант 16 | Пьянкова В. В.";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonPerform;
+    }
+}
+
---
+++ Tyuiu.PyankovaVV.Sprint6.Task0.V16/FormMain.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tyuiu.PyankovaVV.Sprint6.Task0.V16
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
