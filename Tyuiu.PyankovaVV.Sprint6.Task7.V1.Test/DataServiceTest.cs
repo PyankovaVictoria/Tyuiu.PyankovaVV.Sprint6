48687a5711239c0e98de11149eb0d246c5f7d6aa
Tyuiu.PyankovaVV@outlook.com <vika.pyankova04@mail.ru>
2023-12-10 23:08:18 +0500

    Task7

---
+++ Tyuiu.PyankovaVV.Sprint6.Task7.V1.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tyuiu.PyankovaVV.Sprint6.Task7.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\Users\
+            int[,] wait = new int[2, 2] { { 1, 0 },
+                                          { 0, 1 } };
+            int[,] res = ds.GetMatrix(path);
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
